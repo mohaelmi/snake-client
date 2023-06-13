1568eb2986eb1a22b484c77cc0c8aed684263645
mohaelmi <moha.abdi1771@gmail.com>
2023-06-13 17:54:02 -0500

    set up stdin

--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const { connect } = require("./client")
 
 connect()
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput()
+
+
