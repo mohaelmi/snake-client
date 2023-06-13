8b8aacfc0d9b3f632630b11bb1c6c0c60c73dd4b
mohaelmi <moha.abdi1771@gmail.com>
2023-06-13 15:52:21 -0500

    added server code into client.js and updated play.js

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net")
+const { connect } = require("./client")
 
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  })
-
-  conn.setEncoding("utf8")
-
-  conn.on("data", (data) => {
-    console.log(`server sent: ${data}`);
-  })
-
-  return conn
-}
-
-console.log("connecting ...");
 connect()
+
